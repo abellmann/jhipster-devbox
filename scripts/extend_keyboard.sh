0ebe8583bf8ddf563b01cc555458c441aa847e22
Andreas Bellmann <andreas.bellmann@gmail.com>
2017-03-21 20:43:43 +0100

    script to add the extend keyboard navigation of dreymar to this box with the uk standard keyboard layout (see https://github.com/DreymaR/BigBagKbdTrix_XKB.git)

---
+++ scripts/extend_keyboard.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+HomeDir="/home/vagrant"
+git clone https://github.com/DreymaR/BigBagKbdTrix_XKB.git $HomeDir/BigBagKbdTrix_XKB
+sudo bash $HomeDir/BigBagKbdTrix_XKB/install-dreymar-xmod.sh -o
+sudo cp /etc/default/keyboard /etc/default/keyboard.default
+read -d '' DefaultKeyboard <<EOF
+# KEYBOARD CONFIGURATION FILE
+# this uses the extend keyboard navigation from https://github.com/DreymaR/BigBagKbdTrix_XKB
+# with the normal uk english keyboard
+
+XKBMODEL="pc105"
+XKBMODEL="gb"
+XKBVARIANT=""
+XKBOPTIONS="lv5:caps_switch_lock,misc:extend,compose:menu,grp:shifts_toggle"
+
+BACKSPACE="guess"
+EOF
+
+sudo bash -c "echo '$DefaultKeyboard' > /etc/default/keyboard"
+
+read -d '' XkbCommand <<EOF
+
+# Dreymars extended keyboard navigation from https://github.com/DreymaR/BigBagKbdTrix_XKB
+setxkbmap -model 'pc105' -layout 'gb' -option \'misc:extend,lv5:caps_switch_lock,grp:shifts_toggle,compose:menu'
+EOF
+
+bash -c "echo \"$XkbCommand\" >> $HomeDir/.bashrc"
+bash -c "echo \"$XkbCommand\" >> $HomeDir/.zshrc"
