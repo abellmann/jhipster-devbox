2e4073c2ceb38cee88f36a3946d5a0a43871b65b
Andreas Bellmann <andreas.bellmann@gmail.com>
2017-03-24 12:46:05 +0100

    fixed setup script

--- scripts/spacemacs.sh
+++ scripts/spacemacs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo apt-get install emacs
+sudo apt-get install -y emacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 git clone https://github.com/abellmann/emacs.git ~/emacs-config
-ln -s ~/emacs-config/spacemacs/.spacemacs ~/.spacemacs
+ln -sf ~/emacs-config/spacemacs/.spacemacs ~/.spacemacs
+
+rm -rf ~/.emacs.d/private/local/org-ab
 ln -s ~/emacs-config/spacemacs/layers/org-ab ~/.emacs.d/private/local/org-ab
+
+rm -rf ~/.emacs.d/private/local/org-ab-convenience
 ln -s ~/emacs-config/spacemacs/layers/org-ab-convenience ~/.emacs.d/private/local/org-ab-convenience
