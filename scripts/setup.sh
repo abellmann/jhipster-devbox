5f49b1598b8f78911b5b6377eaf6577503316f47
Andreas Bellmann <andreas.bellmann@gmail.com>
2017-03-24 12:45:44 +0100

    git clone does not work with git:// syntax

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -94,14 +94,14 @@ apt-get install -y guake
 cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
 
 # install jhipster-devbox
-git clone git://github.com/jhipster/jhipster-devbox.git /home/vagrant/jhipster-devbox
+git clone https://github.com/jhipster/jhipster-devbox.git /home/vagrant/jhipster-devbox
 chmod +x /home/vagrant/jhipster-devbox/tools/*.sh
 
 # install zsh
 apt-get install -y zsh
 
 # install oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
 chsh -s /bin/zsh vagrant
 echo 'SHELL=/bin/zsh' >> /etc/environment
